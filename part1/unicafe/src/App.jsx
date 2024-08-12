02d8a3e535d6a9f07b21a9c0f15c4276ccf2e9f1
hswinata <harrysurywinata@gmail.com>
2024-08-12 14:20:56 +0200

    finish exercise 1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / all;
+  const positive = all > 0 ? (good / all) * 100 + "%" : 0;
+
+  return (
+    <div>
+      <h3>statistics</h3>
+      <ul>
+        <li>good {good}</li>
+        <li>neutral {neutral}</li>
+        <li>bad {bad}</li>
+        <li>all {all}</li>
+        <li>average {average ? average : 0}</li>
+        <li>positive {positive ? positive : 0}</li>
+      </ul>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const all = good + neutral + bad;
-  const average = (good * 1 + bad * -1) / all;
-  const positive = all > 0 ? (good / all) * 100 + "%" : 0;
 
   const handleGoodClick = () => {
     setGood(good + 1);
@@ -27,17 +43,7 @@ const App = () => {
         <button onClick={handleNeutralClick}> neutral</button>
         <button onClick={handleBadClick}>bad</button>
       </div>
-      <div>
-        <h3>statistics</h3>
-        <ul>
-          <li>good {good}</li>
-          <li>neutral {neutral}</li>
-          <li>bad {bad}</li>
-          <li>all {all}</li>
-          <li>average {average ? average : 0}</li>
-          <li>positive {positive ? positive : 0}</li>
-        </ul>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
