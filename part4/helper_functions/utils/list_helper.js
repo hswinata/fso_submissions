f19d2313601399e0bc4009c017eea98d826b04fe
hswinata <harrysurywinata@gmail.com>
2024-08-28 11:12:40 +0200

    finish exercise 4.4: Helper Functions and Unit Tests, step 2

--- part4/helper_functions/utils/list_helper.js
+++ part4/helper_functions/utils/list_helper.js
@@ -2,4 +2,10 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  return blogs.reduce((sumLikes, blog) => {
+    return sumLikes + blog.likes;
+  }, 0);
+};
+
+module.exports = { dummy, totalLikes };
