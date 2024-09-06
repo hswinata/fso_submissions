d2cecf43e92173808e673e9301754bd56d3274e0
hswinata <harrysurywinata@gmail.com>
2024-09-06 12:33:57 +0200

    5.15: Blog List Tests, step 3

--- part5/bloglist-frontend/src/tests/Blog.test.jsx
+++ part5/bloglist-frontend/src/tests/Blog.test.jsx
@@ -70,3 +70,41 @@ test('url and likes are visible when the "show" button is clicked', async () => 
   // Check that the blog detail div does not have 'display: none'.
   expect(blogDetailDiv).not.toHaveStyle('display: none')
 })
+
+test('Mock event handler is called twice when "like" button is clicked twice', async () => {
+  //Simulate adding blog.
+  const blog = {
+    title: 'Blog component test',
+    author: 'Test Author',
+    url: 'Test URL',
+    likes: 0
+  }
+  const mockLikeHandler = vi.fn()
+  const mockDeleteHandler = vi.fn()
+
+  render(
+    <Blog
+      blog={blog}
+      handleLikeClick={mockLikeHandler}
+      handleDeleteBlog={mockDeleteHandler}
+    />
+  )
+
+  const blogDetailDiv = screen.getByTestId('blog-detail')
+
+  // Initially, the blog detail div should be hidden.
+  expect(blogDetailDiv).toHaveStyle('display: none')
+
+  //Simulate clicking 'show' button
+  const user = userEvent.setup()
+  const showButton = screen.getByRole('button', { name: 'show' })
+  await user.click(showButton)
+
+  //Simulate clicking 'like' button twice
+  const likeButton = screen.getByRole('button', { name: 'like' })
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  //Check if likeButton is clicked twice.
+  expect(mockLikeHandler.mock.calls).toHaveLength(2)
+})
