4d9acb07c7e56529a483512ad99b4eea5468dabe
hswinata <harrysurywinata@gmail.com>
2024-09-09 12:14:36 +0200

    finish exercise 5.18: Blog List End To End Testing, step 2

--- part5/bloglist-E2E/tests/blog_app.spec.js
+++ part5/bloglist-E2E/tests/blog_app.spec.js
@@ -1,18 +1,52 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    // Empty the test database.
+    await request.post("http://localhost:3003/api/testing/reset");
+
+    //Add a new user.
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        name: "testuser",
+        username: "testuser",
+        password: "testuser",
+      },
+    });
+
     await page.goto("http://localhost:5173");
   });
 
   test("Login form is shown", async ({ page }) => {
     const headerText = await page.getByText("log in to application");
-    expect(headerText).toBeVisible();
+    await expect(headerText).toBeVisible();
 
     const username = await page.getByText("username");
-    expect(username).toBeVisible();
+    await expect(username).toBeVisible();
 
     const password = await page.getByText("password");
-    expect(password).toBeVisible();
+    await expect(password).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("Login succeeds with correct credentials", async ({ page }) => {
+      await page.getByTestId("username-input").fill("testuser");
+      await page.getByTestId("password-input").fill("testuser");
+      await page.getByRole("button", { name: "login" }).click();
+
+      await expect(
+        page.getByText("testuser has succesfully logged in")
+      ).toBeVisible();
+    });
+
+    test("Login fails with wrong credentials", async ({ page }) => {
+      await page.getByTestId("username-input").fill("wrong user");
+      await page.getByTestId("password-input").fill("wrong password");
+      await page.getByRole("button", { name: "login" }).click();
+
+      await expect(
+        page.getByText("invalid username or password")
+      ).toBeVisible();
+    });
   });
 });
