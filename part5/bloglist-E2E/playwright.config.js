0e1f37690cf12453117d6b18530da0caf12f35b4
hswinata <harrysurywinata@gmail.com>
2024-09-10 10:25:59 +0200

    finish exercise 5.20: Blog List End To End Testing, step 4

--- part5/bloglist-E2E/playwright.config.js
+++ part5/bloglist-E2E/playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require("@playwright/test");
+const { defineConfig, devices } = require('@playwright/test')
 
 /**
  * Read environment variables from file.
@@ -11,39 +11,39 @@ const { defineConfig, devices } = require("@playwright/test");
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  timeout: 20000,
-  testDir: "./tests",
+  timeout: 10000,
+  testDir: './tests',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
     },
 
     {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
     },
 
     {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
+    }
 
     /* Test against mobile viewports. */
     // {
@@ -64,7 +64,7 @@ module.exports = defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+  ]
 
   /* Run your local dev server before starting the tests */
   // webServer: {
@@ -72,4 +72,4 @@ module.exports = defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+})
--- part5/bloglist-E2E/tests/blog_app.spec.js
+++ part5/bloglist-E2E/tests/blog_app.spec.js
@@ -1,74 +1,79 @@
-const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { login, createBlog } = require('./helper')
 
-describe("Blog app", () => {
+describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
     // Empty the test database.
-    await request.post("http://localhost:3003/api/testing/reset");
+    await request.post('/api/testing/reset')
 
     //Add a new user.
-    await request.post("http://localhost:3003/api/users", {
+    await request.post('/api/users', {
       data: {
-        name: "testuser",
-        username: "testuser",
-        password: "testuser",
-      },
-    });
+        name: 'testuser',
+        username: 'testuser',
+        password: 'testuser'
+      }
+    })
 
-    await page.goto("http://localhost:5173");
-  });
+    await page.goto('/')
+  })
 
-  test("Login form is shown", async ({ page }) => {
-    const headerText = await page.getByText("log in to application");
-    await expect(headerText).toBeVisible();
+  test('Login form is shown', async ({ page }) => {
+    const headerText = await page.getByText('log in to application')
+    await expect(headerText).toBeVisible()
 
-    const username = await page.getByText("username");
-    await expect(username).toBeVisible();
+    const username = await page.getByText('username')
+    await expect(username).toBeVisible()
 
-    const password = await page.getByText("password");
-    await expect(password).toBeVisible();
-  });
+    const password = await page.getByText('password')
+    await expect(password).toBeVisible()
+  })
 
-  describe("Login", () => {
-    test("Login succeeds with correct credentials", async ({ page }) => {
-      await page.getByTestId("username-input").fill("testuser");
-      await page.getByTestId("password-input").fill("testuser");
-      await page.getByRole("button", { name: "login" }).click();
-      const notifDiv = await page.locator(".notification");
-      await expect(notifDiv).toContainText(
-        "testuser has succesfully logged in"
-      );
-    });
+  describe('Login', () => {
+    test('Login succeeds with correct credentials', async ({ page }) => {
+      await login(page, 'testuser', 'testuser')
+      const notifDiv = await page.locator('.notification')
+      await expect(notifDiv).toContainText('testuser has succesfully logged in')
+    })
 
-    test("Login fails with wrong credentials", async ({ page }) => {
-      await page.getByTestId("username-input").fill("wrong user");
-      await page.getByTestId("password-input").fill("wrong password");
-      await page.getByRole("button", { name: "login" }).click();
-      const errorDiv = await page.locator(".error");
-      await expect(errorDiv).toContainText("invalid username or password");
-    });
-  });
+    test('Login fails with wrong credentials', async ({ page }) => {
+      await login(page, 'wronguser', 'wrongpassword')
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('invalid username or password')
+    })
+  })
 
-  describe("When user is logged in", () => {
+  describe('When user is logged in', () => {
     beforeEach(async ({ page }) => {
-      await page.getByTestId("username-input").fill("testuser");
-      await page.getByTestId("password-input").fill("testuser");
-      await page.getByRole("button", { name: "login" }).click();
-    });
+      await login(page, 'testuser', 'testuser')
+    })
 
-    test("User can create a blog", async ({ page }) => {
-      await page.getByRole("button", { name: "new blog" }).click();
-      await page.getByTestId("title-input").fill("test title");
-      await page.getByTestId("author-input").fill("test author");
-      await page.getByTestId("url-input").fill("test url");
-      await page.getByRole("button", { name: "create" }).click();
-      
-      const notifDiv = await page.locator(".notification");
+    test('User can create a blog', async ({ page }) => {
+      await createBlog(page, 'test title', 'test author', 'test url')
+
+      const notifDiv = await page.locator('.notification')
       await expect(notifDiv).toContainText(
-        "a new blog test title has been added"
-      );
+        'a new blog test title has been added'
+      )
 
-      const newBlog = await page.getByText("test title by test author");
-      await expect(newBlog).toBeVisible();
-    });
-  });
-});
+      const newBlog = await page.getByText('test title by test author')
+      await expect(newBlog).toBeVisible()
+    })
+
+    describe('When a user has created one blog', () => {
+      beforeEach(async ({ page }) => {
+        await createBlog(page, 'test title', 'test author', 'test url')
+      })
+
+      test('User can like a blog', async ({ page }) => {
+        await expect(page.getByText('test title by test author')).toBeVisible()
+
+        await page.getByRole('button', { name: 'show' }).click()
+        await page.getByRole('button', { name: 'like' }).click()
+
+        const like = await page.getByText('likes: 1')
+        await expect(like).toBeVisible()
+      })
+    })
+  })
+})
